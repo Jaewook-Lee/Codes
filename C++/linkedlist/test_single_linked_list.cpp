e858ab2c45987a71cc666ea42bdc7e1c2563355a
Jaewook Lee <swljw@kookmin.ac.kr>
2019-04-11 21:48:06 +0900

    I forgot to push these.

---
+++ C++/linkedlist/test_single_linked_list.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "single_linked_list.h"
+using namespace std;
+
+int main()
+{
+    SingleLinkedList list;
+    list.traverse();
+
+    list.insertNode(40);
+    list.insertNode(30);
+    list.traverse();
+
+    list.appendNode(80);
+    list.appendNode(50);
+    list.traverse();
+
+    list.deleteNode(30);
+    list.deleteNode(5);
+    list.traverse();
+
+    list.search(80);
+    list.search(100);
+
+    list.~SingleLinkedList();
+    list.insertNode(160);
+    list.traverse();
+}
