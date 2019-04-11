ee8b5b7be0b8fd36ab737866671c18b432920854
Jaewook Lee <swljw@kookmin.ac.kr>
2019-04-11 23:56:05 +0900

    Comparing between Eulor method and not Eulor method.

--- Python3/bungee_velocity.py
+++ Python3/bungee_velocity.py
@@ -2,36 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-m = np.linspace(40, 200, 100)
-G = 9.8
-Cd = 0.25
+m = 55
+g = 9.8
+cd = 0.25
 
-v = np.sqrt(m*G/Cd)*np.tanh(np.sqrt(G*Cd/m) * 4) # Using Differential Equation
-v1 = np.sqrt(m*G/Cd)*np.tanh(np.sqrt(G*Cd/m) * 4) - 36
-#v0 = 0
-#v1 = v0 + (G - Cd*v0**2/m) # Using Euler's method
-#v2 = v1 + (G - Cd*v1**2/m)
-#v3 = v2 + (G - Cd*v2**2/m)
-#v4 = v3 + (G - Cd*v3**2/m)
-#vel_euler = [v0, v1, v2, v3, v4]
-k = np.linspace(0, 0, 100)
+# Eulor
+v0 = 0
+v1 = (1 - 0)*(g - cd / m * v0**2) + v0
+v2 = (2 - 1)*(g - cd / m * v1**2) + v1
+v3 = (3 - 2)*(g - cd / m * v2**2) + v2
+v4 = (4 - 3)*(g - cd / m * v3**2) + v3
 
-plt.title('Velocities that jumper feels at 4s')
-plt.xlabel('weight (kg)')
-plt.ylabel('velocity (m/s)')
-plt.plot(m, v, 'bo-')
+vel_eulor = [v0, v1, v2, v3, v4]
+
+# Not Eulor(Original)
+vel = []
+for t in range(0, 5):
+    v = np.sqrt(g * m / cd) * np.tanh(np.sqrt(g * cd / m) * t)
+    vel.append(v)
+time = [0, 1, 2, 3, 4]
+
+plt.plot(time, vel, '-ro', label="Original")
+plt.plot(time, vel_eulor, '-bo', label="Eulor")
 plt.grid(True)
+plt.legend()
 plt.show()
-
-plt.title("Weight that doesn't allow")
-plt.xlabel('weight (kg)')
-plt.ylabel('velocity - 36 (m/s)')
-plt.plot(m, v1, 'r*--')
-plt.grid(True)
-plt.show()
-
-plt.figure(2)
-plt.plot(m, v1, m, k)
-plt.grid(True)
-plt.show()
-
